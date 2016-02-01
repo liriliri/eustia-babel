7d2bed282596456664424a1ef3ace1796753e13f
surunzi <surunzi@foxmail.com>
2016-02-01 19:13:05 +0800

    Dev: Small changes

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-var babel = require('babel-core');
+var babel = require('babel-core'),
+    _ = require('./lib/util');
 
-module.exports = function (src)
+var defOpts = {
+    presets: ['es2015']
+};
+
+module.exports = function (options)
 {
-    return babel.transform(src, {
-        presets: ['es2015']
-    }).code;
+    _.defaults(options, defOpts);
+
+    return function (src)
+    {
+        return babel.transform(src, options).code;
+    };
 };
